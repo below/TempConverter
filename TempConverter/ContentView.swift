0c13bc177acc0563191d3e5419523cfeb39e72ca
Alexander von Below <alexander.von_below@telekom.de>
2020-10-22 20:50:18 +0200

    Initial Commit

---
+++ TempConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TempConverter
+//
+//  Created by Alexander v. Below on 22.10.20.
+//  Copyright © 2020 Alexander von Below. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TempConverterTests/TempConverterTests.swift
@@ -0,0 +1,34 @@
+//
+//  TempConverterTests.swift
+//  TempConverterTests
+//
+//  Created by Alexander v. Below on 22.10.20.
+//  Copyright © 2020 Alexander von Below. All rights reserved.
+//
+
+import XCTest
+@testable import TempConverter
+
+class TempConverterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
